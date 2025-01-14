02ae000e832be170fe02a232c4ae8956f8b1f669
Elcio <elcio@visie.com.br>
2025-01-14 10:33:28 -0300

    Tornando mais fácil criar um admin configurado.

--- apps/_default/superadmin/controllers.py
+++ apps/_default/superadmin/controllers.py
@@ -6,20 +6,21 @@ from .settings import APP_NAME, TEMPLATE_FOLDER
 from ..common import (T, db, flash, requires_membership, menu)
 
 
+TABLES = db.tables
 menu.set(APP_NAME, [1000, 'Super Admin', (APP_NAME, 'index'), 'superadmin', []])
 
 
 @action(f"{APP_NAME}/index")
 @action.uses(f"{TEMPLATE_FOLDER}index.html", requires_membership('superadmin'), T, flash)
 def index():
-    return dict(tables=db.tables, T=T, menu=menu, app_name=APP_NAME)
+    return dict(tables=TABLES, T=T, menu=menu, app_name=APP_NAME)
 
 
 @action(f"{APP_NAME}/table/<table>")
 @action(f"{APP_NAME}/table/<table>/<path:path>")
 @action.uses(f"{TEMPLATE_FOLDER}table.html", requires_membership('superadmin'), T, flash)
 def table(table, path=None):
-    if table not in db.tables:
+    if table not in TABLES:
         abort(404)
     grid = Grid(
         path,
